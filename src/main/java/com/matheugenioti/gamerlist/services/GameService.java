816fb090680892428e15cd85e51ab79721246064
Matheus Eugenio <math.eugenioti@gmail.com>
2023-07-12 13:43:50 -0300

    search game by id

--- src/main/java/com/matheugenioti/gamerlist/services/GameService.java
+++ src/main/java/com/matheugenioti/gamerlist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.matheugenioti.gamerlist.dto.GameDTO;
 import com.matheugenioti.gamerlist.dto.GameMinDTO;
 import com.matheugenioti.gamerlist.entities.Game;
 import com.matheugenioti.gamerlist.repositories.GameRepository;
@@ -14,6 +16,14 @@ public class GameService {
 	
 	@Autowired
 	private GameRepository gameRepository;
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+	
 	public List<GameMinDTO>findAll(){
 		List<Game> result = gameRepository.findAll();
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
